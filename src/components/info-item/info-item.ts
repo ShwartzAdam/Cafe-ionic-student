dba00a0a3a867e4c2b7a5656a91777bb336b0ff1
Adam Shwartz <adam@shwartz.co>
2018-06-07 17:11:15 +0300

    changesss

---
+++ src/components/info-item/info-item.ts
@@ -0,0 +1,23 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Item} from "../../model/item";
+import {ItemProvider} from "../../providers/item/item";
+
+@Component({
+  selector: 'info-item',
+  templateUrl: 'info-item.html'
+})
+export class InfoItemComponent implements OnInit{
+  @Input() itemInput: number;
+  private item: Item = new Item;
+
+  constructor(private itemPro: ItemProvider) {
+  }
+
+  ngOnInit(): void {
+    console.log(this.itemInput);
+    this.itemPro.getItemById(this.itemInput).subscribe(
+      res => this.item = res
+    )
+  }
+
+}
