7fb042741907f94d92451c23e9f00bc294feb9a3
Adam Shwartz <adam@shwartz.co>
2018-08-13 20:32:15 +0300

    Heroku Dpeloyment.

--- src/directives/img-preload/img-preload.ts
+++ src/directives/img-preload/img-preload.ts
@@ -16,19 +16,23 @@ export class ImagePreloader implements OnInit {
   downloadingImage : any; // In class holder of remote image
   finalImage: any; //property bound to our host attribute.
 
-  @Input() defaultImage : string = 'assets/imgs/drinkk.jpg';
+  @Input() defaultImage : string = 'assets/imgs/item.png';
 
   ngOnInit() {
     //First set the final image to some default image while we prepare our preloader:
 
     this.downloadingImage = new Image();  // create image object
     // console.log(this.targetSource);
-    this.userPro.getImage(this.targetSource).subscribe(res => {
-      if(res != null){
-        // console.log("image exist");
-        this.createImageFromBlob(res);
-      }
-    },err => {this.finalImage = this.defaultImage});
+    if(!this.targetSource){
+      this.finalImage = this.defaultImage
+    } else{
+      this.userPro.getImage(this.targetSource).subscribe(res => {
+        if(res != null){
+          // console.log("image exist");
+          this.createImageFromBlob(res);
+        }
+      },err => {this.finalImage = this.defaultImage});
+    }
 
   }
   createImageFromBlob(image: Blob) {
